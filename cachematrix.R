51721bd6cd8e4d950a1cd0a90b64d03b963ced11
Mike <michaelmurphy602@gmail.com>
2015-12-22 15:14:16 -0800

    Functions written and tested locally.  Comments added.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## Functions return the inversion of a given matrix.  As matrix inversion
+## may use significant amounts of memory, function first checks to see if
+## the calculation previously was done and saved in a cache.
 
-## Write a short comment describing this function
+## First function creates a list of functions and places the given matrix
+## in the cache for later inversion
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function (x=matrix()) {
+  m <- NULL
+  setmat <- function (y)  {
+    
+    x <<- y
+    m<<-NULL
+  }
+  getmat <- function () x
+  setmatinvert <- function (solve)  m <<- solve  ##function inverts matrix
+  getmatinvert <- function() m                   ##retrieves inversion
+  list(setmat = setmat,
+       getmat = getmat,
+       setmatinvert = setmatinvert,
+       getmatinvert = getmatinvert)
+  
 }
 
+## Second function calculates the inversion unless the calculation has 
+## been performed and placed in cache.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+  s <- x$getmatinvert()
+  ## retrieve, if exists, cached invert
+  if(!is.null(s)) {
+    message("Retrieving cached invert")
+    return(s)
+    
+  }
+  ## checks for cached invert, if exists, messages and uses invert
+  s <- x$getmat()   ## null cached invert, retrieves initials matrix
+  v <- solve(s)     ## solves(inverts) retrieved matrix
+  x$setmatinvert(v)  ## place solution in cache
+  v   ## output the solution
+  
 }
